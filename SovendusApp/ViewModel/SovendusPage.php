dfd4f31cc3d2737ebccf07d5326a5eb495eedeed
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-01-27 17:02:50 +0100

    fix landing page script

--- SovendusApp/ViewModel/SovendusPage.php
+++ SovendusApp/ViewModel/SovendusPage.php
@@ -4,21 +4,17 @@ namespace Sovendus\SovendusApp\ViewModel;
 
 use Magento\Framework\App\ObjectManager;
 use Sovendus\SovendusApp\Model\Config;
+use Magento\Framework\View\Element\Template;
 
 require_once __DIR__ . '/../sovendus-plugins-commons/settings/get-settings-helper.php';
 require_once __DIR__ . '/../sovendus-plugins-commons/helpers/integration-data-helpers.php';
 require_once __DIR__ . '/../Constants.php';
 require_once __DIR__ . '/helper.php';
 
-
 class SovendusPage
 {
-
     public static function get_sovendus_page_settings()
     {
-
-
-
         [$language, $country] = detectLanguage();
         $objectManager = ObjectManager::getInstance();
         $configModel = $objectManager->get(Config::class);
