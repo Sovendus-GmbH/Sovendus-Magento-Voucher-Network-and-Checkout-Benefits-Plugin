361372a3583609d92d22418e37e03f92e2e14b00
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-07-16 12:17:36 +0200

    update package.json and add release zip files for version 2.1.0

--- sov_build.config.ts
+++ sov_build.config.ts
@@ -4,22 +4,22 @@ const buildConfig: BuildConfig = {
   filesToCompile: [
     {
       sovOptions: {
-        input: "SovendusApp/view/adminhtml/web/js/frontend_react_loader.ts",
-        output: "SovendusApp/view/adminhtml/web/js/frontend_react_loader.js",
+        input: "./SovendusApp/view/adminhtml/web/js/frontend_react_loader.ts",
+        output: "./SovendusApp/view/adminhtml/web/js/frontend_react_loader.js",
         type: "react-tailwind",
       },
     },
     {
       sovOptions: {
-        input: "SovendusApp/view/frontend/web/ts/sovendus-page.ts",
-        output: "SovendusApp/view/frontend/web/js/sovendus-page.js",
+        input: "./SovendusApp/view/frontend/web/ts/sovendus-page.ts",
+        output: "./SovendusApp/view/frontend/web/js/sovendus-page.js",
         type: "vanilla",
       },
     },
     {
       sovOptions: {
-        input: "SovendusApp/view/frontend/web/ts/thankyou-page.ts",
-        output: "SovendusApp/view/frontend/web/js/thankyou-page.js",
+        input: "./SovendusApp/view/frontend/web/ts/thankyou-page.ts",
+        output: "./SovendusApp/view/frontend/web/js/thankyou-page.js",
         type: "vanilla",
       },
     },
